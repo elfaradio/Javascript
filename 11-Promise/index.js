49b55966c817c2c73be974b698c65387f2cc9598
elfaradio <farhadulislam84054@gmail.com>
2025-10-01 19:00:11 +0600

    js

--- 11-Promise/index.js
+++ 11-Promise/index.js
@@ -89,7 +89,7 @@ consumePromiseFive();
 
 //getAllUsers()
 
-fetch("https://api.github.com/users/hiteshchoudhary")
+fetch("https://api.github.com/users/elfaradio")
   .then((response) => {
     return response.json();
   })
