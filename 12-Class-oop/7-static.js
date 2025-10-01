10889e0f8c38f4444e6f7f5d487e42be6b6e5695
elfaradio <farhadulislam84054@gmail.com>
2025-10-01 20:36:40 +0600

    bind

---
+++ 12-Class-oop/7-static.js
@@ -0,0 +1,27 @@
+class User {
+  constructor(username) {
+    this.username = username;
+  }
+
+  logMe() {
+    console.log(`Username: ${this.username}`);
+  }
+
+  static createId() {
+    // cannot be accessed by instance
+    return `123`;
+  }
+}
+
+const far = new User("far");
+// console.log(far.createId())
+
+class Teacher extends User {
+  constructor(username, email) {
+    super(username);
+    this.email = email;
+  }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com");
+console.log(iphone.createId());
